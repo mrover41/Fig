13ff1be395d0165826ecd1823d59078218954d4c
mrover41 <mrover41@users.noreply.github.com>
2026-09-25 16:40:38 +0300

    base code

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cstdint>
+#include<sys/types.h>
+
+#include "Process.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	cout << "Input PID: " << enld;
+
+	if (argc < 2) return 1;
+	pid_t pid = stoi(argv[1]);
+
+	//TODO: Code
+}
